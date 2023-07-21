fdb41ddf48a80212cbfd1f6a46422d8ff87f5067
haiche <cheviethai123@gmail.com>
2023-07-21 15:59:02 +0700

    update service

--- services/service-common/tests/test_main.py
+++ services/service-common/tests/test_main.py
@@ -2,9 +2,16 @@ import io
 from unittest import mock
 
 from service_common.main import main
+import runpy
 
 
 @mock.patch("sys.stdout", new_callable=io.StringIO)
 def test_stdout(mock_stdout):
     main()
     assert mock_stdout.getvalue().strip() == "Go for opentelemetry"
+
+@mock.patch("sys.stdout", new_callable=io.StringIO)
+def test_execute_file(mock_stdout):
+    runpy._run_module_as_main("service_common.main")
+    assert mock_stdout.getvalue().strip() == "Go for opentelemetry"
+
