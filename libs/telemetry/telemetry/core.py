a5dc24b4c79bf26bc6d2e53d36a67906dab0f9db
hai che <cheviethai123@gmail.com>
2023-07-28 22:40:41 +0700

    fix lint

--- libs/telemetry/telemetry/core.py
+++ libs/telemetry/telemetry/core.py
@@ -3,6 +3,7 @@ from opentelemetry import trace
 tracer = trace.get_tracer("__name__")
 
 
-@tracer.start_as_current_span("main")
 def main() -> None:
-    print("Go for opentelemetry")
+    with tracer.start_as_current_span("main") as rollspan:
+        print("Go for opentelemetry")
+        rollspan.set_attribute("roll", 1)
