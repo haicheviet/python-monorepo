13d7f8d7af501de08d4121b38035723bbab79a41
hai che <cheviethai123@gmail.com>
2023-07-29 12:21:40 +0700

    update deploy package

--- services/fastapi-ml/scripts/build.sh
+++ services/fastapi-ml/scripts/build.sh
@@ -6,6 +6,16 @@ TAG=${TAG:=latest}
 INSTALL_TEST=${INSTALL_TEST:=false}
 
 
+# Get latest image to reuse compile stage
+if [ "$(docker images -q $DOCKER_IMAGE:compile-stage-$TAG 2>/dev/null)" != "" ]; then
+       TAG_LATEST=$TAG
+       echo "Get exist img"
+else
+       docker pull $DOCKER_IMAGE:compile-stage-$TAG_LATEST || true
+       echo "Get latest img"
+fi
+
+
 # Build the compile stage:
 docker buildx build --file Dockerfile \
        --target compile-image \
@@ -14,7 +24,7 @@ docker buildx build --file Dockerfile \
        --build-context telemetry=../../libs/telemetry \
        --build-context ml=../../libs/ml \
        --build-context mq=../../libs/mq \
-       --cache-from $DOCKER_IMAGE:compile-stage-$TAG \
+       --cache-from $DOCKER_IMAGE:compile-stage-$TAG_LATEST \
        --tag $DOCKER_IMAGE:compile-stage-$TAG .
 
 
